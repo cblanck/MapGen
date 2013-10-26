7fc457c7e59083585615d990945351f9276ab8c4
Connor Blanck <connorblanck@gmail.com>
2013-10-25 23:39:55 -0400

    Added isometricism

--- display.py
+++ display.py
@@ -5,10 +5,15 @@ from tile import Tile
 window_x = 640
 window_y = 640
 
-tiles_x = 20
-tiles_y = 20
+tile_width = 96
+tile_height = 48
+x_start = -tile_width
+y_start = -tile_height
 
-tile_names = ["grass1.jpg", "grass2.jpg", "grass3.jpg", "flower.jpg", "tree.jpg", "stump.jpg"]
+tiles_x = int(window_x/tile_width)*2
+tiles_y = int(window_y/tile_height)*2
+
+tile_names = ["tile_grass.png"]
 
 def main():
     pygame.init()
@@ -18,12 +23,12 @@ def main():
     screen = pygame.display.get_surface()
     clock = pygame.time.Clock()
     tile_group = pygame.sprite.Group()
-    tiles = [[None]*tiles_x]*tiles_y
+    tiles = [[None]*tiles_y]*tiles_x
     for i, ts in enumerate(tiles):
         for j, tile in enumerate(ts):
             tiles[i][j] = Tile(tile_group, image = random.choice(tile_names))
-            tiles[i][j].rect.x = window_x/tiles_x * i
-            tiles[i][j].rect.y = window_y/tiles_y * j
+            tiles[i][j].rect.x = x_start + (tile_width/2)*i
+            tiles[i][j].rect.y = y_start + (tile_height/2)*j
     while 1:
         time_passed = clock.tick(60)
         for event in pygame.event.get():
--- tile.py
+++ tile.py
@@ -1,8 +1,8 @@
 import pygame, os
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, group, image = "flower.jpg"):
+    def __init__(self, group, image = "tile_grass.png"):
         pygame.sprite.Sprite.__init__(self, group)
-        self.image = pygame.image.load(os.path.join('tiles', image)).convert()
+        self.image = pygame.image.load(os.path.join('tiles', image))
         self.rect = self.image.get_rect()
          
